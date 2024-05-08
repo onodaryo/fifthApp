d18f9093652f1c28e0bf27a82ea496f0dce79f98
ryoonoda <onoda.ryo@genetec.co.jp>
2024-05-08 11:59:10 +0900

    新アイテム追加のためのテキストボックスについてバリデーション機能の追加

--- lib/widgets/newItem.dart
+++ lib/widgets/newItem.dart
@@ -5,7 +5,6 @@ import 'package:flutter/widgets.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 
-
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
   @override
@@ -28,42 +27,83 @@ class _NewItemState extends State<NewItem> {
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: const InputDecoration(label: Text('Name'),),
-                validator: (value){
-                  return '';
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  // value はテキストボックス内に入力されている内容
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null; // correct!
                 },
               ),
               Row(
                 children: [
                   Expanded(
                     child: TextFormField(
-                      decoration: const InputDecoration(label: Text('Quantity'),),
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
                       initialValue: '1',
+                      validator: (value) {
+                        // value はテキストボックス内に入力されている内容
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be valid, positive number.';
+                        }
+                        return null; // correct!
+                      },
                     ),
                   ),
-                  const SizedBox(width: 8,),
+                  const SizedBox(
+                    width: 8,
+                  ),
                   Expanded(
                     child: DropdownButtonFormField(
                       items: [
                         for (final category in categories.entries)
-                               DropdownMenuItem(
-                                 value: category.value,
-                                 child: Row(
-                                   children: [
-                                     Container(
-                                       width: 16,
-                                       height: 16,
-                                       color: category.value.categoryColor,
-                                     ),
-                                     const SizedBox(width: 6,),
-                                     Text(category.value.categoryName),
-                                   ],
-                                 ),
-                               ),
-                          ],
-                      onChanged: (value){},
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.categoryColor,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.categoryName),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
                     ),
                   )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Item'),
+                  ),
                 ],
               ),
             ],
